b2c7c481aa946e2e1bc430aea6ed7e3617750f9f
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2023-02-07 23:06:53 -0300

    Aplicando os Wedgets conhecidos até o momento

--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,60 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.teal,
-            height: 300,
-            width: 30,
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.teal,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.deepOrange,
+                height: 50,
+                width: 50,
+              ),
+            ],
           ),
-          Container(
-            color: Colors.deepOrange,
-            height: 300,
-            width: 300,
-          )
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.deepOrange,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.teal,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: Colors.amberAccent,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.cyanAccent,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.deepPurpleAccent,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
         ],
       ),
     );
