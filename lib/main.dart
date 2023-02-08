dd479cb56e50f229c5d504c169b33f800964ead7
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2023-02-08 11:56:38 -0300

    Solução desafio alura screen presente em Tests/"Test 1.PNG"

--- lib/main.dart
+++ lib/main.dart
@@ -16,78 +16,113 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        color: Colors.white,
+      // home: Container(
+      //   color: Colors.white,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Stack(
+      //         alignment: AlignmentDirectional.center,
+      //         children: [
+      //           Container(
+      //             color: Colors.teal,
+      //             height: 100,
+      //             width: 100,
+      //           ),
+      //           Container(
+      //             color: Colors.deepOrange,
+      //             height: 50,
+      //             width: 50,
+      //           ),
+      //         ],
+      //       ),
+      //       Stack(
+      //         alignment: AlignmentDirectional.center,
+      //         children: [
+      //           Container(
+      //             color: Colors.deepOrange,
+      //             height: 100,
+      //             width: 100,
+      //           ),
+      //           Container(
+      //             color: Colors.teal,
+      //             height: 50,
+      //             width: 50,
+      //           ),
+      //         ],
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         children: [
+      //           Container(
+      //             color: Colors.amberAccent,
+      //             height: 50,
+      //             width: 50,
+      //           ),
+      //           Container(
+      //             color: Colors.cyanAccent,
+      //             height: 50,
+      //             width: 50,
+      //           ),
+      //           Container(
+      //             color: Colors.deepPurpleAccent,
+      //             height: 50,
+      //             width: 50,
+      //           ),
+      //         ],
+      //       ),
+      //       Container(
+      //         color: Colors.amber,
+      //         height: 30,
+      //         width: 300,
+      //         child: const Text(
+      //           'Hello World!',
+      //           textAlign: TextAlign.center,
+      //           style: TextStyle(
+      //             fontSize: 28,
+      //             color: Colors.black,
+      //           ),
+      //         ),
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: () => print('Pressed Button'),
+      //         child: const Text('Press Button'),
+      //       )
+      //     ],
+      //   ),
+      // ),
+      home: Container( // Build Screen Test/'Test 1.PNG'
+        color: Colors.black,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.teal,
-                  height: 100,
-                  width: 100,
-                ),
-                Container(
-                  color: Colors.deepOrange,
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.deepOrange,
-                  height: 100,
-                  width: 100,
-                ),
-                Container(
-                  color: Colors.teal,
-                  height: 50,
-                  width: 50,
-                ),
+                Container(color: Colors.deepOrange, height: 100, width: 100),
+                Container(color: Colors.deepPurple, height: 100, width: 100),
+                Container(color: Colors.greenAccent, height: 100, width: 100),
               ],
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.amberAccent,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.cyanAccent,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.deepPurpleAccent,
-                  height: 50,
-                  width: 50,
-                ),
+                Container(color: Colors.deepOrange, height: 100, width: 100),
+                Container(color: Colors.deepPurple, height: 100, width: 100),
+                Container(color: Colors.greenAccent, height: 100, width: 100),
               ],
             ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: const Text(
-                'Hello World!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () => print('Pressed Button'),
-              child: const Text('Press Button'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(color: Colors.deepOrange, height: 100, width: 100),
+                Container(color: Colors.deepPurple, height: 100, width: 100),
+                Container(color: Colors.greenAccent, height: 100, width: 100),
+              ],
             )
           ],
         ),
