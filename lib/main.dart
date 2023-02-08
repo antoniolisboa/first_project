c80cbecb09dddb801b850bc9c5bc3609451e9ef2
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2023-02-08 10:37:58 -0300

    Conhecendo o Widget Text e aplicando texto em um Container

--- lib/main.dart
+++ lib/main.dart
@@ -11,65 +11,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.teal,
-                height: 100,
-                width: 100,
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.teal,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.deepOrange,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.teal,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  color: Colors.amberAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.cyanAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.deepPurpleAccent,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Hello World!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                ),
               ),
-              Container(
-                color: Colors.deepOrange,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.deepOrange,
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                color: Colors.teal,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                color: Colors.amberAccent,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.cyanAccent,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.deepPurpleAccent,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
