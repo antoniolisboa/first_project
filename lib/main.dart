ef50c8d0ffcd38ca09006cb1500a864de97511e1
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2023-02-08 15:12:22 -0300

    Minha primeira tela em flutter [Aplicando o que aprendi até o momento]

--- lib/main.dart
+++ lib/main.dart
@@ -16,113 +16,78 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: Container(
-      //   color: Colors.white,
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //     crossAxisAlignment: CrossAxisAlignment.center,
-      //     children: [
-      //       Stack(
-      //         alignment: AlignmentDirectional.center,
-      //         children: [
-      //           Container(
-      //             color: Colors.teal,
-      //             height: 100,
-      //             width: 100,
-      //           ),
-      //           Container(
-      //             color: Colors.deepOrange,
-      //             height: 50,
-      //             width: 50,
-      //           ),
-      //         ],
-      //       ),
-      //       Stack(
-      //         alignment: AlignmentDirectional.center,
-      //         children: [
-      //           Container(
-      //             color: Colors.deepOrange,
-      //             height: 100,
-      //             width: 100,
-      //           ),
-      //           Container(
-      //             color: Colors.teal,
-      //             height: 50,
-      //             width: 50,
-      //           ),
-      //         ],
-      //       ),
-      //       Row(
-      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //         children: [
-      //           Container(
-      //             color: Colors.amberAccent,
-      //             height: 50,
-      //             width: 50,
-      //           ),
-      //           Container(
-      //             color: Colors.cyanAccent,
-      //             height: 50,
-      //             width: 50,
-      //           ),
-      //           Container(
-      //             color: Colors.deepPurpleAccent,
-      //             height: 50,
-      //             width: 50,
-      //           ),
-      //         ],
-      //       ),
-      //       Container(
-      //         color: Colors.amber,
-      //         height: 30,
-      //         width: 300,
-      //         child: const Text(
-      //           'Hello World!',
-      //           textAlign: TextAlign.center,
-      //           style: TextStyle(
-      //             fontSize: 28,
-      //             color: Colors.black,
-      //           ),
-      //         ),
-      //       ),
-      //       ElevatedButton(
-      //         onPressed: () => print('Pressed Button'),
-      //         child: const Text('Press Button'),
-      //       )
-      //     ],
-      //   ),
-      // ),
-      home: Container( // Build Screen Test/'Test 1.PNG'
-        color: Colors.black,
+      home: Container(
+        color: Colors.white,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            Stack(
+              alignment: AlignmentDirectional.center,
               children: [
-                Container(color: Colors.deepOrange, height: 100, width: 100),
-                Container(color: Colors.deepPurple, height: 100, width: 100),
-                Container(color: Colors.greenAccent, height: 100, width: 100),
+                Container(
+                  color: Colors.teal,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.deepOrange,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.deepOrange,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.teal,
+                  height: 50,
+                  width: 50,
+                ),
               ],
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(color: Colors.deepOrange, height: 100, width: 100),
-                Container(color: Colors.deepPurple, height: 100, width: 100),
-                Container(color: Colors.greenAccent, height: 100, width: 100),
+                Container(
+                  color: Colors.amberAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.cyanAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.deepPurpleAccent,
+                  height: 50,
+                  width: 50,
+                ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.deepOrange, height: 100, width: 100),
-                Container(color: Colors.deepPurple, height: 100, width: 100),
-                Container(color: Colors.greenAccent, height: 100, width: 100),
-              ],
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Hello World!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Pressed Button'),
+              child: const Text('Press Button'),
             )
           ],
         ),
