952eb53484d5220aeddc276ea652219d53009909
Antonio Carlos de Lisboa <antoniolisboa@alunos.utfpr.edu.br>
2023-02-23 11:36:32 -0300

    [Project Task List] Conhecendo o Widget AnimatedOpacity e aplicando na ListView para ocultar e mostrar as tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isOpacity = true;
+  IconData visibilityIcon = Icons.visibility_outlined;
 
   // This widget is the root of your application.
   @override
@@ -31,42 +39,61 @@ class MyApp extends StatelessWidget {
               fontSize: 22,
             ),
           ),
-        ),
-        body: ListView(
-          children: const [
-            Task(
-              taskName: 'Aprender Flutter',
-              image:
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              difficulty: 3,
-            ),
-            Task(
-              taskName: 'Andar de Bike',
-              image:
-                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-              difficulty: 2,
-            ),
-            Task(
-              taskName: 'Meditar',
-              image:
-                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-              difficulty: 1,
-            ),
-            Task(
-              taskName: 'Ler',
-              image:
-                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-              difficulty: 3,
-            ),
-            Task(
-              taskName: 'Jogar',
-              image: 'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
-              difficulty: 0,
-            ),
-            SizedBox(
-              height: 75,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: visibilityOnOff,
+                icon: Icon(
+                  visibilityIcon,
+                  color: Colors.black54,
+                ),
+              ),
             )
           ],
+        ),
+        body: AnimatedOpacity(
+          opacity: isOpacity ? 1 : 0,
+          duration: const Duration(
+            seconds: 1,
+          ),
+          child: ListView(
+            children: const [
+              Task(
+                taskName: 'Aprender Flutter',
+                image:
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                difficulty: 3,
+              ),
+              Task(
+                taskName: 'Andar de Bike',
+                image:
+                    'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                difficulty: 2,
+              ),
+              Task(
+                taskName: 'Meditar',
+                image:
+                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                difficulty: 1,
+              ),
+              Task(
+                taskName: 'Ler',
+                image:
+                    'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                difficulty: 3,
+              ),
+              Task(
+                taskName: 'Jogar',
+                image:
+                    'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+                difficulty: 0,
+              ),
+              SizedBox(
+                height: 75,
+              )
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.greenAccent,
@@ -78,6 +105,14 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void visibilityOnOff() {
+    setState(() {
+      isOpacity = !isOpacity;
+      visibilityIcon =
+          isOpacity ? Icons.visibility_outlined : Icons.visibility_off_outlined;
+    });
   }
 }
 
